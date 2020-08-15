a863f50095f8ba93121c0e7ed7f73542440bc9f6
rizkasalmaadelia <rizkasalmaadelia@gmail.com>
2020-08-15 10:15:29 +0700

    feat: add data favorite toko

---
+++ src/data/toko.js
@@ -0,0 +1,50 @@
+const data = [
+    {
+        favoriteToko : [
+            {
+                "id": 1,
+                "productName": "Tas Kulit Bambu",
+                "productPrice": "5000",
+                "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597418743/desaku/image/toko/toko-1_lkuhte.png",
+                "starCount": "4.5"
+            },
+            {
+                "id": 2,
+                "productName": "Tas Kulit Bambu",
+                "productPrice": "5000",
+                "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597418743/desaku/image/toko/toko-1_lkuhte.png",
+                "starCount": "4.5"
+            },
+            {
+                "id": 3,
+                "productName": "Tas Kulit Bambu",
+                "productPrice": "5000",
+                "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597418743/desaku/image/toko/toko-1_lkuhte.png",
+                "starCount": "4.5"
+            },
+            {
+                "id": 4,
+                "productName": "Tas Kulit Bambu",
+                "productPrice": "5000",
+                "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597418743/desaku/image/toko/toko-1_lkuhte.png",
+                "starCount": "4.5"
+            },
+            {
+                "id": 5,
+                "productName": "Tas Kulit Bambu",
+                "productPrice": "5000",
+                "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597418743/desaku/image/toko/toko-1_lkuhte.png",
+                "starCount": "4.5"
+            },
+            {
+                "id": 6,
+                "productName": "Tas Kulit Bambu",
+                "productPrice": "5000",
+                "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597418743/desaku/image/toko/toko-1_lkuhte.png",
+                "starCount": "4.5"
+            }
+        ]
+    } 
+]
+
+export default data;
