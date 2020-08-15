268151d4a8dbd2063b30d2dab400e6b135a7722a
rizkasalmaadelia <rizkasalmaadelia@gmail.com>
2020-08-15 15:46:24 +0700

    feat: add data toko

--- src/data/dataToko.js
+++ src/data/dataToko.js
@@ -48,25 +48,32 @@ const data = [
     {
         favoriteToko : [
             {
-                "id": 1,
-                "productName": "Bola Takraw",
-                "productPrice": "5000",
-                "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597418743/desaku/image/toko/toko-1_lkuhte.png",
-                "starCount": "4.5"
-            },
-            {
-                "id": 2,
-                "productName": "Kursi Mini",
-                "productPrice": "5000",
-                "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597418725/desaku/image/toko/toko-2_czr7xv.png",
-                "starCount": "4.2"
-            },
-            {
                 "id": 3,
                 "productName": "Topi Anyaman",
                 "productPrice": "5000",
                 "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597418727/desaku/image/toko/toko-3_fkqjbl.png",
                 "starCount": "4.6"
+            },
+            {
+                "id": 1,
+                "productName": "Lukisan Abstrak",
+                "productPrice": "105000",
+                "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597480447/desaku/image/toko/lukisan-abstrak_bgd2ca.png",
+                "starCount": "4.5"
+            },
+            {
+                "id": 2,
+                "productName": "Cangkir Cantik",
+                "productPrice": "15000",
+                "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597480449/desaku/image/toko/cangkir-cantik_qctziq.png",
+                "starCount": "4.2"
+            },
+            {
+                "id": 6,
+                "productName": "Tikar Plastik",
+                "productPrice": "5000",
+                "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597418720/desaku/image/toko/toko-6_biwyll.png",
+                "starCount": "5.0"
             },
             {
                 "id": 4,
@@ -77,28 +84,28 @@ const data = [
             },
             {
                 "id": 5,
-                "productName": "Piring Lidi",
+                "productName": "Mangkok Gerabah",
                 "productPrice": "5000",
-                "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597418762/desaku/image/toko/toko-5_uxov3x.png",
+                "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597480451/desaku/image/toko/gerabah_hb98dp.png",
                 "starCount": "4.9"
             },
-            {
-                "id": 6,
-                "productName": "Tikar Plastik",
-                "productPrice": "5000",
-                "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597418720/desaku/image/toko/toko-6_biwyll.png",
-                "starCount": "5.0"
-            }
         ]
     },
     {
         favoriteToko : [
             {
-                "id": 1,
-                "productName": "Bola Takraw",
+                "id": 5,
+                "productName": "Mangkok Gerabah",
                 "productPrice": "5000",
-                "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597418743/desaku/image/toko/toko-1_lkuhte.png",
-                "starCount": "4.5"
+                "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597480451/desaku/image/toko/gerabah_hb98dp.png",
+                "starCount": "4.9"
+            },
+            {
+                "id": 6,
+                "productName": "Tikar Plastik",
+                "productPrice": "5000",
+                "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597418720/desaku/image/toko/toko-6_biwyll.png",
+                "starCount": "5.0"
             },
             {
                 "id": 2,
@@ -108,13 +115,6 @@ const data = [
                 "starCount": "4.2"
             },
             {
-                "id": 3,
-                "productName": "Topi Anyaman",
-                "productPrice": "5000",
-                "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597418727/desaku/image/toko/toko-3_fkqjbl.png",
-                "starCount": "4.6"
-            },
-            {
                 "id": 4,
                 "productName": "Keset Serabut",
                 "productPrice": "5000",
@@ -122,18 +122,18 @@ const data = [
                 "starCount": "4.4"
             },
             {
+                "id": 1,
+                "productName": "Lukisan Abstrak",
+                "productPrice": "105000",
+                "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597480447/desaku/image/toko/lukisan-abstrak_bgd2ca.png",
+                "starCount": "4.5"
+            },
+            {
                 "id": 5,
                 "productName": "Piring Lidi",
                 "productPrice": "5000",
                 "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597418762/desaku/image/toko/toko-5_uxov3x.png",
                 "starCount": "4.9"
-            },
-            {
-                "id": 6,
-                "productName": "Tikar Plastik",
-                "productPrice": "5000",
-                "productImage": "https://res.cloudinary.com/sarjanalidi/image/upload/v1597418720/desaku/image/toko/toko-6_biwyll.png",
-                "starCount": "5.0"
             }
         ]
     }
