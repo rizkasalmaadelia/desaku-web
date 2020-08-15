af180a793b27dc1adc4628c0dbc4d305b1becb46
putuayu202 <dwikydanob123@gmail.com>
2020-08-15 19:20:34 +0700

    add pengiriman

---
+++ src/pages/pengiriman/pengiriman.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { Router, Route, Link } from "svelte-routing";
+	import { onMount } from "svelte";
+    import Navbar from "../../components/navbar";
+    import Footer from "../../components/footer";
+    import CardToko from "../../components/cardToko";
+    import CardPembelajaran from "../../components/cardPembelajaran";
+    import CardPembelajaranOffline from "../../components/cardPembelajaranOffline";
+    import * as dataHome from "../../data/dataHome";
+</script>
+
+<body>
+    <p>ppppp</p>
+</body>
